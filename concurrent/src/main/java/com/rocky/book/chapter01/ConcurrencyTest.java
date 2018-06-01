c3cb76582a137a16e476eef260339ffb35f35f5e
chenqun <chenqun93@qq.com>
2018-06-01 09:43:33 +0800

    解决乱码，编码更改为utf8

--- concurrent/src/main/java/com/rocky/book/chapter01/ConcurrencyTest.java
+++ concurrent/src/main/java/com/rocky/book/chapter01/ConcurrencyTest.java
@@ -1,22 +1,33 @@
-package main.java.com.rocky.concurrency;
+package com.rocky.book.chapter01;
 
 /**
- * Created by Administrator on 2018/5/31.
+ * 并发和单线程执行测试
+ *
+ * @author tengfei.fangtf
+ * @version $Id: ConcurrencyTest.java, v 0.1 2014-7-18 下午10:03:31 tengfei.fangtf Exp $
  */
 public class ConcurrencyTest {
+
+    /** 执行次数 */
+    private static final long count = 10000l;
+
     public static void main(String[] args) throws InterruptedException {
+        //并发计算
         concurrency();
+        //单线程计算
         serial();
     }
 
-    private static final long count = 100000001;
-
     private static void concurrency() throws InterruptedException {
         long start = System.currentTimeMillis();
-        Thread thread = new Thread(() -> {
-            int a = 0;
-            for (long i = 0; i < count; i++) {
-                a += 5;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+                System.out.println(a);
             }
         });
         thread.start();
@@ -26,8 +37,7 @@ public class ConcurrencyTest {
         }
         thread.join();
         long time = System.currentTimeMillis() - start;
-        System.out.println("concurrency : " + time + "ms,b=" + b);
-
+        System.out.println("concurrency :" + time + "ms,b=" + b);
     }
 
     private static void serial() {
@@ -41,7 +51,7 @@ public class ConcurrencyTest {
             b--;
         }
         long time = System.currentTimeMillis() - start;
-        System.out.println("serial : " + time + "ms,b=" + b + ",a=" + a);
-
+        System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
     }
+
 }
